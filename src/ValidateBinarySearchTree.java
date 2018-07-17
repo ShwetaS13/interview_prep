cefab60e5a3e20a6a89c6bcdccc4dc6d47f63098
ShwetaS13 <shweta.sukumaran@gmail.com>
2018-07-17 18:08:13 -0400

    added new class for ValidateBinarySearchTree

---
+++ src/ValidateBinarySearchTree.java
@@ -0,0 +1,14 @@
+public class ValidateBinarySearchTree {
+
+    public static void main(String args[]) {
+
+    }
+
+    public boolean validateBinarySearchTree(TreeNode node) {
+        if (node == null) {
+            return false;
+        }
+
+        return true;
+    }
+}
